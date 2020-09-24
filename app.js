64d9ba1115a4f9999828c5ed745e548ef79f95d5
Savag3life <unknowngaming878@gmail.com>
2020-09-23 19:15:43 -0700

    Fixes

--- app.js
+++ app.js
@@ -1,34 +1,38 @@
 var express = require('express');
-var Steam = require('steam');
-
+var SteamUser = require('steam-user');
 var app = express();
+var spawn = require('child_process');
 
 app.use(function (req, res, next) {
   console.log('%s %s', req.method, req.url);
   next()
 });
 
-app.get('/convert/:username-:password', async (req, res) => {
-  var steamClient = new Steam.SteamClient();
-  var steamUser = new Steam.SteamUser(steamClient);
+app.get('/convert/:username&:password', (req, res) => {
+  var client = new SteamUser();
   console.log('a Attempting to login with username: ' + req.params.username + ' password: ' + req.params.password);
-  steamClient.connect();
-  steamClient.on('connected', function () {
-    steamUser.logOn({
-      account_name: req.params.username,
-      password: req.params.password
-    });
+
+  client.logOn({
+    "accountName": req.params.username,
+    "password": req.params.password
   });
-  steamClient.on('logOnResponse', function (log) {
-    if (log.eresult === Steam.EResult.OK) {
-      res.end(JSON.stringify(log));
-      steamClient.disconnect();
-    } else {
-      res.end(JSON.stringify({client_supplied_steamid: 0}));
-      steamClient.disconnect();
-    }
+  
+
+  client.on('loggedOn', function(details) {
+    console
+    console.log(JSON.stringify(details));
+    console.log("Logged into Steam as " + client.steamID.getSteamID64());
+    res.end(JSON.stringify({ response: "good", id: client.steamID.getSteamID64() }))
   });
+
+  client.on('error', function(e) {
+    res.end(JSON.stringify({ response: "bad", id: 0 }));
+  });
+
+});
+
+process.on('uncaughtException', function (err) {
+  console.log("Error logging into steam...");
 });
 
 app.listen(3000);
-module.exports = app;
